3384e62e2fe560b13df43108daaa37954978c325
samyakkhade321-arch <samyakkhade321@gmail.com>
2025-10-28 14:21:56 +0530

    Create Globle_and_local_variable.cpp

---
+++ Globle_and_local_variable.cpp
@@ -0,0 +1,15 @@
+# include <iostream.h>
+
+int m = 10; // global m
+void main ( ) { 
+  int m = 20; //m redeclared, local to main 
+  int k =m; 
+  int m = 30; //m declared again
+
+// local to inner block 
+  cout <<"'we are in inner block "<<endl; 
+  cout <<"The value of K is : " <<k<<endl;
+  cout <<"The value of local M is : " <<m<<endl;
+  cout <<" global m = "<< ::m<<endl;
+
+} }
